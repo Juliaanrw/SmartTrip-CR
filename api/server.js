b9d026add8f2a1950b06644de98fba7d5e67bb22
Julian <julianeroes@gmail.com>
2026-07-20 13:40:30 -0600

    configuracion base de servidor express, dotenv y conexion a mongodb

---
+++ api/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//Middlewares Básicos
+app.use(cors());
+app.use(express.json());
+
+//Conexión a MongoDB usando Variables de Entorno
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('Conexión exitosa a MongoDB desde variables de entorno'))
+  .catch((error) => console.error('Error al conectar a MongoDB:', error));
+
+//Ruta de prueba inicial
+app.get('/', (req, res) => {
+  res.send('Servidor Base de SmartTrip CR Operando Correctamente');
+});
+
+//Puerto de escucha
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor de SmartTrip corriendo en: http://localhost:${PORT}`);
+});
