acc8e4097ad12f49b133de0c5ada88b3b6b102f8
vidalcarlosesteban-ship-it <vidalcarlosesteban@gmail.com>
2026-07-21 14:54:46 -0600

    Crear modelo de usuarios

--- api/models/Usuario.js
+++ api/models/Usuario.js
@@ -1,1 +1,43 @@
+const mongoose = require("mongoose");
 
+const usuarioSchema = new mongoose.Schema(
+  {
+    correo: {
+      type: String,
+      required: [true, "El correo es obligatorio"],
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+
+    contrasena_hash: {
+      type: String,
+      required: [true, "La contraseña es obligatoria"]
+    },
+
+    rol: {
+      type: String,
+      required: [true, "El rol es obligatorio"],
+      trim: true,
+      default: "turista"
+    },
+
+    estado: {
+      type: String,
+      required: [true, "El estado es obligatorio"],
+      trim: true,
+      default: "activo"
+    },
+
+    fecha_creacion: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    collection: "usuarios",
+    versionKey: false
+  }
+);
+
+module.exports = mongoose.model("Usuario", usuarioSchema);
