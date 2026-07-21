b1963ed245b0b492663d9cd9a88535b800e43f24
vidalcarlosesteban-ship-it <vidalcarlosesteban@gmail.com>
2026-07-21 15:15:55 -0600

    Add itinerarioRoutes to server configuration

--- api/server.js
+++ api/server.js
@@ -5,15 +5,21 @@ const mongoose = require('mongoose');
 const lugarRoutes = require("./routes/lugarRoutes");
 const usuarioRoutes = require("./routes/usuarioRoutes");
 const perfilRoutes = require("./routes/perfilRoutes");
+const itinerarioRoutes = require("./routes/itinerarioRoutes");
+
+
+
 
 const app = express();
-
 //Middlewares Básicos
 app.use(cors());
 app.use(express.json());
 app.use("/api/lugares", lugarRoutes);
 app.use("/api/usuarios", usuarioRoutes);
 app.use("/api/perfiles", perfilRoutes);
+app.use("/api/itinerarios", itinerarioRoutes);
+
+
 
 //Conexión a MongoDB usando Variables de Entorno
 const MONGO_URI = process.env.MONGO_URI;
