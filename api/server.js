d000148611ef6ec3fdaa6120357216a6cc034ab0
vidalcarlosesteban-ship-it <vidalcarlosesteban@gmail.com>
2026-07-21 14:58:02 -0600

    Conectar rutas de usuarios al servidor

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const lugarRoutes = require("./routes/lugarRoutes");
+const usuarioRoutes = require("./routes/usuarioRoutes");
 
 const app = express();
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/lugares", lugarRoutes);
+app.use("/api/usuarios", usuarioRoutes);
 
 //Conexión a MongoDB usando Variables de Entorno
 const MONGO_URI = process.env.MONGO_URI;
