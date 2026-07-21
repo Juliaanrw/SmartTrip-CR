50f1291942973b7e1428ae4dae6ac0a3e38fd255
vidalcarlosesteban-ship-it <vidalcarlosesteban@gmail.com>
2026-07-21 15:30:01 -0600

    Crear modelo de presupuestos

--- api/models/Presupuesto.js
+++ api/models/Presupuesto.js
@@ -1,1 +1,34 @@
+const mongoose = require("mongoose");
 
+const presupuestoSchema = new mongoose.Schema(
+  {
+    itinerario_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Itinerario",
+      required: [true, "El itinerario es obligatorio"]
+    },
+
+    monto_maximo_colones: {
+      type: mongoose.Schema.Types.Decimal128,
+      required: [true, "El monto máximo es obligatorio"]
+    },
+
+    total_gastado: {
+      type: mongoose.Schema.Types.Decimal128,
+      required: [true, "El total gastado es obligatorio"],
+      default: 0
+    },
+
+    descripcion_gastos: {
+      type: String,
+      required: [true, "La descripción de los gastos es obligatoria"],
+      trim: true
+    }
+  },
+  {
+    collection: "presupuestos",
+    versionKey: false
+  }
+);
+
+module.exports = mongoose.model("Presupuesto", presupuestoSchema);
